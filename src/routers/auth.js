90d817a26ebdfa5c321f3b7c30beef48dfe66996
iahub4metra <buhai.artem7@gmail.com>
2024-10-30 19:39:35 +0000

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserController } from '../controllers/auth.js';
+import { loginUserController, registerUserController } from '../controllers/auth.js';
+import { loginUserSchema, registrationUserSchema } from '../validation/auth.js';
+import { validateBody} from "../utils/validateBody.js"
 
 const authRouter = Router();
 
-authRouter.post('/register', ctrlWrapper(registerUserController));
+authRouter.post('/register', validateBody(registrationUserSchema), ctrlWrapper(registerUserController));
+
+authRouter.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController))
 
 export default authRouter;
