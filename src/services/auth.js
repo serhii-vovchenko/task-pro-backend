3058badb4f04c2a923b50897a45049de3c841993
iahub4metra <buhai.artem7@gmail.com>
2024-10-30 19:39:20 +0000

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,17 +1,41 @@
 import createHttpError from "http-errors"
-import { UsersCollection } from "../db/models/user"
+import { UsersCollection } from "../db/models/user.js"
 import bcrypt from "bcrypt"
-
+import {SessionsCollection} from "../db/models/session.js"
+import { randomBytes } from "crypto"
+import { THIRTY_DAYS, TWO_HOURS } from "../constants/auth.js"
 
 export const registerUser = async (registrationData) => {
-    const user = UsersCollection.findOne({ email: registrationData.email })
+    const user = await UsersCollection.findOne({ email: registrationData.email })
     
-    if (!user) throw createHttpError(409, "Email in use")
+    if (user) throw createHttpError(409, "Email in use")
 
-    const encryptedPwd = bcrypt.hash(registrationData.password, 10)
+    const encryptedPwd = await bcrypt.hash(registrationData.password, 10)
 
     return await UsersCollection.create({
         ...registrationData,
         password: encryptedPwd
+    })
+}
+
+
+export const loginUser = async (loginData) => {
+    const user = await UsersCollection.findOne({ email: loginData.email })
+    if (!user) throw createHttpError(401, "User not found")
+
+    const isEqual = bcrypt.compare(loginData.password, user.password)
+    if (!isEqual) throw createHttpError(401, "Unauthorized!")
+
+    await SessionsCollection.deleteOne({ userId: user.id })
+    
+    const accessToken = randomBytes(30).toString('base64')
+    const refreshToken = randomBytes(30).toString('base64')
+
+    return await SessionsCollection.create({
+        userId: user.id,
+        accessToken,
+        refreshToken,
+        accessTokenValidUntil: new Date(Date.now() + TWO_HOURS),
+        refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS)
     })
 }
