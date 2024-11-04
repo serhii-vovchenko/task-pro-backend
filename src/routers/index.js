19f00a92b310bb532a1671afeca1578cf47ab9cc
ZorAnderius <gabriel.zorander@gmail.com>
2024-11-04 17:09:51 +0000

    change route from boardId/columns to columns

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import authRouter from './auth.js';
 import userRouter from './user.js';
-// import columnsRouter from './columns.js';
+import columnsRouter from './columns.js';
 import helpMailRouter from './help.js';
 import boardsRouter from './boards.js';
 // import tasksRouter from './tasks.js';
@@ -11,7 +11,7 @@ const router = Router();
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/boards', boardsRouter);
-// router.use('/columns', columnsRouter);
+router.use('/columns', columnsRouter);
 // router.use('/tasks', tasksRouter);
 router.use('/', helpMailRouter);
 
