cf927cb446e6860a6e60b10c2594d52a878660de
Alina-ps <alinaserg2014@gmail.com>
2024-11-05 20:38:01 +0100

    added trim to boards and columns

--- src/validation/columns.js
+++ src/validation/columns.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const createColumnSchema = Joi.object({
-  title: Joi.string().min(3).max(60).required().messages({
+  title: Joi.string().min(3).max(60).trim().required().messages({
     'any.required': 'Title name is required.',
     'string.empty': 'Title name cannot be empty.',
     'string.min': 'Title name should be at least 3 characters long.',
@@ -14,7 +14,7 @@ export const createColumnSchema = Joi.object({
 });
 
 export const updateColumnSchema = Joi.object({
-  title: Joi.string().min(3).max(60).messages({
+  title: Joi.string().min(3).max(60).trim().messages({
     'string.empty': 'Title name cannot be empty.',
     'string.min': 'Title name should be at least 3 characters long.',
     'string.max': 'Title name should not exceed 60 characters.',
